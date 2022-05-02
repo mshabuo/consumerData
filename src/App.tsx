8a27b0dc61ea4cbd86a078a8ff63ea6223a5721d
“mshabuo” <moe.shabuo@hotmail.co.uk>
2022-05-02 20:52:49 +0100

    Basic CSS grid added and app components created and imported to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import "./App.css"
+import Header from "./Components/Header"
+import Main from "./Components/Main"
+import Footer from "./Components/Footer"
+import ConsumerCard from "./Components/ConsumerCard"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <ConsumerCard />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
